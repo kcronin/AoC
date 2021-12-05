14dd132a06a037e61c53521c3e9a654079566e82
Kate Cronin <ccronin@etsy.com>
2021-12-05 22:26:46 +0000

    cleanup and consolidation

--- 2021/5.py
+++ 2021/5.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-# every point has coordinates
-# top left is 0,0
-# bottom right is max(columns), max(rows)
-# {'coord': 'times_crossed'} ?
-
 example = """
 0,9 -> 5,9
 8,0 -> 0,8
@@ -18,55 +13,53 @@ example = """
 5,5 -> 8,2
 """
 
-example_result = 5
-
 #data = example
 data = open('5.input', 'r').read()
 
-considered_lines = []
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def get_points(self):
+        points = []
+        start_x, start_y, end_x, end_y = self.start[0], self.start[1], self.end[0], self.end[1]
+        x_movement = range(start_x, end_x) if start_x <= end_x else range(start_x, end_x, -1)
+        y_movement = range(start_y, end_y) if start_y <= end_y else range(start_y, end_y, -1)
+        if len(x_movement) == 0: # vertical
+            for m in y_movement:
+                points.append((start_x, m))
+        elif len(y_movement) == 0: # horizontal
+            for m in x_movement:
+                points.append((m, start_y))
+        else: # diag
+            points = list(zip(x_movement, y_movement))
+        points.append(self.end)
+        return points
+
+def get_total_points(lines, ignore_diag=True):
+    total_points_crossed = {}
+    for line in lines:
+        diagonal = (line[0][0] != line[1][0] and line[0][1] != line[1][1])
+        if ignore_diag and diagonal:
+            continue
+        points_crossed = Line(line[0], line[1]).get_points()
+        for point in points_crossed:
+            total_points_crossed[point] = total_points_crossed.get(point, 0) + 1
+    return len([v for v in total_points_crossed.values() if v > 1])
+
+def part_one(lines):
+    return get_total_points(lines)
+
+def part_two(lines):
+    return get_total_points(lines, ignore_diag=False)
+
+lines = []
 
 for line in data.split("\n"):
     if len(line) > 0:
         start, finish = line.split(" -> ")
-        start, finish = eval(start), eval(finish)
-        if start[0] == finish[0] or start[1] == finish[1]:
-            considered_lines.append([start, finish])
-#print(considered_lines)
+        lines.append([eval(start), eval(finish)])
 
-# x = across
-# y = down
-# considered_lines = [[(0,9), (5,9)], [(9,4), (3,4)], [(2,4), (2,1)]]
-
-# points crossed by considered_lines[0] are 0-5,9 or 0,9; 1,9; etc thru 5,9
-# points crossed by considered_lines[2] are 2,4-1 or 2,4; 2,3; 2,2; 2,1 <-- note this was an "upward" movement, need to figure that out
-
-total_points_crossed = {}
-# this will look like {(0,9): 1, (1,9): 2 ... }
-
-def get_points(line):
-    points = []
-    start_x, start_y, end_x, end_y = line[0][0], line[0][1], line[1][0], line[1][1]
-    inc = 1
-    if start_x == end_x: #vert
-        if start_y > end_y:
-            inc = -1
-        for movement in range(start_y, end_y, inc):
-            points.append((start_x, movement))
-    else: #horiz
-        if start_x > end_x:
-            inc = -1
-        for movement in range(start_x, end_x, inc):
-            points.append((movement, start_y))
-    points.append(line[1])
-    return points
-
-
-for line in considered_lines:
-    points_crossed = get_points(line)
-    for point in points_crossed:
-        total_points_crossed[point] = total_points_crossed.get(point, 0) + 1
-
-#print(total_points_crossed)
-# find all values greater than 1
-result = len([v for v in total_points_crossed.values() if v > 1])
-print(result)
+print(f"Part one: {part_one(lines)}")
+print(f"Part two: {part_two(lines)}")
