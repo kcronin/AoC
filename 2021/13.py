a273a0d3699fd55f2b85f4b834d0f08f7c1d9a50
Kate Cronin <ccronin@etsy.com>
2021-12-14 21:44:18 +0000

    removed some extraneous crap

--- 2021/13.py
+++ 2021/13.py
@@ -21,31 +21,10 @@ data = """
 9,0
 """
 
-#data = """
-#0,0
-#0,1
-#0,3
-#1,4
-#2,0
-#3,0
-#3,4
-#4,1
-#4,3
-#6,0
-#6,2
-#6,4
-#8,4
-#9,0
-#9,4
-#10,2
-#10,4
-#"""
-
 folds = """
 y=7
 x=5
 """
-
 
 data = open('13.input', 'r').read()
 folds = open('13.folds', 'r').read()
@@ -65,14 +44,6 @@ for point in data:
         grid[int(y)].append(int(x))
 
 def y_fold(grid, foldline, height): # horizonal fold upwards
-#    halfway = math.ceil(statistics.median(range(height)))
-#    if foldline < halfway: # we will have extra rows at the top after the fold
-#        extra_rows = height - foldline * 2
-#        # move bottom x rows to top x rows
-#        for i in range(extra_rows):
-#            grid[i] = grid[height - i] 
-
-    #else:
     for i in range(1, foldline+1):
         try:
             grid[foldline - i] = list(set(grid[foldline - i] + grid[foldline + i]))
@@ -83,10 +54,6 @@ def y_fold(grid, foldline, height): # horizonal fold upwards
     return grid, height
 
 def x_fold(grid, foldline, width):
-    #halfway = max_x / 2
-    #if foldline < halfway: # we will have extra columns on the left after the fold
-    #    extra_cols = (halfway - foldline) * 2
-    #else:
     for line in grid:
         for i, x in enumerate(grid[line]):
             if x > foldline:
@@ -110,10 +77,9 @@ for fold in folds:
             visible_points += len(i)
         print(f"Visible points after this fold: {visible_points}")
 
-for line in grid.values():
-    if len(line) > 0:
-        final_line = ['.' for i in range(width)]
-        for x in line:
-            final_line[x] = '#'
-        print(''.join(final_line))
-
+#for line in grid.values():
+#    if len(line) > 0:
+#        final_line = ['.' for i in range(width)]
+#        for x in line:
+#            final_line[x] = '#'
+#        print(''.join(final_line))
