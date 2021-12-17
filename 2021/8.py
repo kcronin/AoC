60013ff9d9758a22dd5ed0f77a4b5be16bd50347
Kate Cronin <ccronin@etsy.com>
2021-12-17 01:09:03 +0000

    Adding day 8 part 2 finally

--- 2021/8.py
+++ 2021/8.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
 
-# 0 = 6
-# 1 = 2 *
-# 2 = 5
-# 3 = 5
-# 4 = 4 *
-# 5 = 5
-# 6 = 6
-# 7 = 3 *
-# 8 = 7 *
-# 9 = 6
-
-# 1 (2), 4 (4), 7 (3) , 8 (7) are unique
-
 data = """
 be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -26,86 +13,56 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 """
 
-data = open('8.input', 'r').readlines()
+data = open('8.input', 'r').read()
 
 total_unique = 0
 
-for line in data:
-    print(line)
+for line in data.split("\n"):
     if len(line) > 0:
-        digits = line.split('|')[1].split()
+        digits = line.split(' | ')[1].split()
         for d in digits:
             if len(d) in [2, 4, 3, 7]:
                 total_unique += 1
 
-print(total_unique)
-
-digit_codes = [
-    'abcefg', # 0 len(6)
-    'cf',     # 1 len(2)
-    'acdeg',  # 2 len(5) 
-    'acdfg',  # 3 len(5)
-    'bcdf',   # 4 len(4)
-    'abdfg',  # 5 len(5)
-    'abdefg', # 6 len(6)
-    'acf',    # 7 len(3)
-    'abcdefg',# 8 len(7)
-    'abcdfg', # 9 len(6)
-    ]
-
-# be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb  cefbgd gcbe
-   1       8      9      6    4     5      0     3     2   7         8     3       9    4
-
-# first, check if right-hand of | is known-unique numbers (1, 4, 8, 7), if so, continue
-# first, find easy targets (1, 4, 7, 8) on both sides
-# then move onto len5s on left hand side, for each one discovered, check against len5s on right side
-# same w/ len6s until all numbers accounted for
-
-unique_digits = {
-    2: 1,
-    3: 7,
-    4: 4,
-    7: 8
-    }
+print(f"Part one: {total_unique}")
 
 total = 0
 
-for line in data:
-    result = {}
-    left_side, right_side = line.split(' | ')
-    for i, digit in enumerate(right_side.split()):
-        if len(digit.strip()) in unique_digits:
-            result[i] = unique_digits[len(digit.strip())]
-    if len(result) == 4:
-        total += int(''.join(result.values()))
-        continue
-    else:
-        deciphered = {}
-        for i, digit in enumerate(left_side.split()):
-            if len(digit.strip()) in unique_digits:
-                deciphered[i] = digit.strip()
-            elif len(digit.strip()) == 5:
-                possibilities_5 = [2,3,5]
-                # do we contain a 1?
-                if digit.strip().find(deciphered[1][0]
+for line in data.split("\n"):
+    result = []
+    if len(line) > 0:
+        left_side, right_side = line.split(' | ')
+        for digit in left_side.split():
+            if len(digit) == 2:
+                one = set(digit)
+            elif len(digit) == 4:
+                four = set(digit)
 
+        for digit in right_side.split():
+            if len(digit) == 2:
+                num = 1
+            elif len(digit) == 3:
+                num = 7
+            elif len(digit) == 4:
+                num = 4
+            elif len(digit) == 5:
+                if len(one & set(digit)) == 2:
+                    num = 3
+                elif len(four & set(digit)) == 2:
+                    num = 2
+                else:
+                    num = 5
+            elif len(digit) == 6:
+                if len(one & set(digit)) == 1:
+                    num = 6
+                elif len(four & set(digit)) == 4:
+                    num = 9
+                else:
+                    num = 0
+            else:
+                num = 8
 
-        possibilities_5 = [2, 3, 5]
+            result.append(num)
+        total += int(''.join(map(str, result)))
 
-
-
-
-# cefdb can only be 3 because of the three posibilities, it is the only one with b and e
-# cefbgd has be, so can only be 0 or 9
-# can't be zero because it shares all letters with 4, so 9
-# fdcge (len5) could be 2, 3 or 5
-# doesn't have be so can't be 3
-# closest known with all same letters is 9, so 5
-# so now we know the remaining 5-letter combo is 2
-# fgaecd (len 6) remaining options are 0 and 6
-# do we have be? no - cannot be 0, must be 6
-
-
-
-
-
+print(f"Part two: {total}")
