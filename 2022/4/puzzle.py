3559b7cfa07da96412b61e96e1f38775a8d78900
Kate Cronin <ccronin@etsy.com>
2022-12-04 18:56:35 -0800

    Make better

--- 2022/4/puzzle.py
+++ 2022/4/puzzle.py
@@ -3,29 +3,30 @@
 with open('input.txt') as f:
     input = f.readlines()
 
-# there are probably stdlibs to do these range comparisons but ¯\_(ツ)_/¯
+def prep_input():
+    elves_a = []
+    elves_b = []
+    for pair in input:
+        a, b = pair.strip().split(',')
+        elves_a.append([int(x) for x in a.split('-')])
+        elves_b.append([int(x) for x in b.split('-')])
+    return zip(elves_a, elves_b)
 
-def part_one():
-    total = 0
-    for i in input:
-        i = i.strip()
-        a, b = i.split(',')
-        if ((int(a.split('-')[0]) >= int(b.split('-')[0]) and int(a.split('-')[1]) <= int(b.split('-')[1])) or
-           (int(a.split('-')[0]) <= int(b.split('-')[0]) and int(a.split('-')[1]) >= int(b.split('-')[1]))):
-            total += 1
-    return total
-
-def part_two():
-    total = 0
-    for i in input:
-        i = i.strip()
-        a, b = i.split(',')
-        for z in range(int(a.split('-')[0]), int(a.split('-')[1])+1):
-            if z in range(int(b.split('-')[0]), int(b.split('-')[1])+1):
-                total += 1
+def find_overlaps(elves):
+    total_1 = 0
+    total_2 = 0
+    for pair in elves:
+        # part one
+        if ((pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]) or
+            (pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1])):
+            total_1 += 1
+        for z in range(pair[0][0], pair[0][1]+1):
+            if z in range(pair[1][0], pair[1][1]+1):
+                total_2 += 1
                 break
-    return total
+
+    return total_1, total_2 # 471, 888
 
 if __name__ == "__main__":
-    print(part_one()) # 471
-    print(part_two()) # 888
+    elves = prep_input()
+    print(find_overlaps(elves))
