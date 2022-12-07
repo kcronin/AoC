350e727ae7a73532447aeca588409cc1b656629a
Kate Cronin <ccronin@etsy.com>
2022-12-06 20:33:22 -0800

    day 6 rethought

--- 2022/6/puzzle.py
+++ 2022/6/puzzle.py
@@ -3,6 +3,7 @@
 with open('input.txt') as f:
     letters = f.read().strip()
 
+# initial solution
 def parse_letters(num):
     for i, c in enumerate(letters):
         if i < num-1:
@@ -12,5 +13,18 @@ def parse_letters(num):
         print(f"Found {letters[i-(num-1):i+1]} at position {i+1}.")
         break
 
-parse_letters(4) # 1566
-parse_letters(14) # 2265
+# rethought solution
+def parse_letters2(num):
+    i = num
+    while i < len(letters):
+        candidate = letters[i-num:i]
+        if len(set(candidate)) != num:
+            i += 1
+            continue
+        print(i)
+        break
+
+#parse_letters(4) # 1566
+#parse_letters(14) # 2265
+parse_letters2(4)
+parse_letters2(14)
